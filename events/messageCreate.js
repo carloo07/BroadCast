0773c36f3a4181f1263e1cf56cbad0141fbac08b
carloo07 <76703454+carloo07@users.noreply.github.com>
2022-04-08 02:02:23 +0300

    Add files via upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,28 @@
 
+    const client = require("../index");
+const { prefix } = require('../config.js')
+client.on("messageCreate", async (message) => {
+
+  if (
+       message.author.bot ||
+       !message.guild)return;
+
+        var prefixx = prefix
+
+       if(!message.content.toLowerCase().startsWith(prefix))return;
+
+const arg = message.content
+.slice(prefix.length)
+.split(/ +/g); 
+const commandName = arg.shift().toLowerCase();
+if (commandName.length === 0) return;
+let command = client.commands.get(commandName);
+if (!command) return;
+
+  
+
+
+await command.run(client, message,arg, prefixx);
+     
+
+});
